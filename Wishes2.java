8aeaf97e64ab98488c97a514d7c2e02b5283aeab
Atharva_Dev <113286938+Atharva-Nimbalkar@users.noreply.github.com>
2023-07-31 15:12:53 +0530

    Methods (Concept) in Java

---
+++ Wishes2.java
@@ -0,0 +1,15 @@
+package com.atharva;
+
+public class Wishes2 {
+    public static void main(String[] args) {
+        wish();
+        wish();
+        wish();
+        wish();
+    }
+//static which does not depend on objects
+    static void wish(){
+        System.out.println("Hello folks");
+    }
+
+}
