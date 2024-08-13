8fe9daea409862340019ba852cc889df1ed94aa6
Atharva_Dev <113286938+Atharva-Nimbalkar@users.noreply.github.com>
2024-08-14 00:02:36 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-
-    }
-//    in stack push,pop,top has O(1) TC.
-//    stack can implement using Array,arraylist,linkedlist
-//    top in Linedlist will be head;
-}
